d6803c97f22ed6e45f6712c49698fc3b7752fb3c
IN-FOMO <it.zero.developer@gmail.com>
2025-12-13 00:55:38 +0800

    Исправил ошибки безопасности и доступа на сервер через WAF next.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  turbopack: {
+    // Явно задаём корень workspace, чтобы Next не "угадывал" его по чужим lockfile'ам
+    root: __dirname,
+  },
   images: {
     remotePatterns: [
       {
